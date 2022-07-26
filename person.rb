6ead82fb9ccf0a58a6b7c2b041b6680ad85aae61
medsonmoombe <medsonmoombe21@gmail.com>
2022-07-26 13:06:19 +0200

    Fix linters

--- person.rb
+++ person.rb
@@ -27,9 +27,3 @@ class Person < Nameable
 
   private :of_age?
 end
-person = Person.new(22, 'maximilianus')
- puts person.correct_name
-  capitalizedPerson = CapitalizeDecorator.new(person)
- puts capitalizedPerson.correct_name
-  capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
- puts capitalizedTrimmedPerson.correct_name
