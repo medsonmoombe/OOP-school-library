c88e2a2cb4f76955575a8d4e076a31fdcdb210f5
medsonmoombe <medsonmoombe21@gmail.com>
2022-07-26 12:25:40 +0200

    Add the methods and classesin the decorator and nameable

--- person.rb
+++ person.rb
@@ -1,8 +1,14 @@
-class Person
+# rubocop: disable Style/OptionalBooleanParameter
+
+require_relative './nameable'
+require_relative './decorator'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
+    super()
     @id = Random.rand(1..1000)
     @age = age
     @name = name
@@ -13,9 +19,20 @@ class Person
     @age >= 18
   end
 
+  def correct_name
+    @name
+  end
+
   def can_use_services?
     of_age? || @parent_permission
   end
 
   private :of_age?
 end
+person = Person.new(22, 'maximilianus')
+  puts person.correct_name
+  capitalizedPerson = CapitalizeDecorator.new(person)
+  puts capitalizedPerson.correct_name
+  capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+  puts capitalizedTrimmedPerson.correct_name
+# rubocop: enable Style/OptionalBooleanParameter
