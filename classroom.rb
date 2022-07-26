d8ae2cfa749499071805a1a2cc047510d2659452
medsonmoombe <medsonmoombe21@gmail.com>
2022-07-26 22:19:26 +0200

    Add the classroom class with the methods

---
+++ classroom.rb
@@ -0,0 +1,13 @@
+class Classroom
+  attr_accessor : label
+  attr_reader : students
+
+  def initialize(label)
+    @label = label
+    @students = []
+
+    def add_student(student)
+        @students << student
+        student.classroom = self
+    end
+end
